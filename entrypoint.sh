f29b56af450e775b3be6b7856d072656b40a385f
ron c project <ron.c.project@gmail.com>
2026-09-21 13:30:49 +0200

    Removed SEED, entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ nginx
 
 # 2. Start App 1 (Hardened Hack Game) in the foreground with Seed 20
 # Moved to 'exec' to keep the container alive and expanded memory allocation to a safe 512MB
-exec java -Dserver.port=8081 -Xmx512m -jar /app/app1.jar --SEED 20
+exec java -Dserver.port=8081 -Xmx512m -jar /app/app1.jar 
